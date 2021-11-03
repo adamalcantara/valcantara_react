aa9fb9902caf1ceaa654f308f8ed9a8a454306b1
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-02 23:34:21 -0400

    Added media queries for stuff section

--- src/Pages/StuffToDo/StuffToDo.js
+++ src/Pages/StuffToDo/StuffToDo.js
@@ -12,7 +12,7 @@ const StuffToDo = () => {
             <h1 id="attractionHeader">Things to Do</h1>
             <div id="touristSection">
                 <div id="henryford" className="touristCard">
-                    <a href="https://www.thehenryford.org/"><h2 className="touristTitle">The Henry Ford Museum</h2></a>
+                    <a href="https://www.thehenryford.org/" className="touristTitle"><h2>The Henry Ford Museum</h2></a>
                     <img src={HenryFord} alt="The Henry Ford Museum" className="attractionimg"></img>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -21,7 +21,7 @@ const StuffToDo = () => {
                     culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
                 <div id="hitsville" className="touristCard">
-                    <a href="https://www.motownmuseum.org/"><h2 className="touristTitle">The Motown Museum</h2></a>
+                    <a href="https://www.motownmuseum.org/" className="touristTitle"><h2>The Motown Museum</h2></a>
                     <img src={Motown} alt="Motown Museum" className="attractionimg"></img>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -30,7 +30,7 @@ const StuffToDo = () => {
                     culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
                 <div id="hitsville" className="touristCard">
-                    <a href="https://www.dia.org/"><h2 className="touristTitle">The Detroit Institute of Art</h2></a>
+                    <a href="https://www.dia.org/" className="touristTitle"><h2>The Detroit Institute of Art</h2></a>
                     <img src={DIA} alt="DIA" className="attractionimg"></img>
                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
