7075de6fe8c60ef22209e304bc2b3a329ddc30ba
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-04 09:05:47 -0400

    Added timeline

--- src/Pages/OurStory/OurStory.js
+++ src/Pages/OurStory/OurStory.js
@@ -12,7 +12,30 @@ const OurStory = () => {
             <h1 id="storyHeader">Our Story</h1>
 
             <div id="storySection">
-                <div id="timeline"></div>
+                <div id="timeline">
+                    <ul className="sessions">
+                        <li>
+                            <div className="date">August 2018</div>
+                            <p>First Met</p>
+                        </li>
+                        <li>
+                            <div className="date">November 2018</div>
+                            <p>First Worked Together</p>
+                        </li>
+                        <li>
+                            <div className="date">February 2020</div>
+                            <p>Started Hanging Out Outside Work</p>
+                        </li>
+                        <li>
+                            <div className="date">March 2020</div>
+                            <p>First Date</p>
+                        </li>
+                        <li>
+                            <div className="date">August 2021</div>
+                            <p>Got Engaged</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
