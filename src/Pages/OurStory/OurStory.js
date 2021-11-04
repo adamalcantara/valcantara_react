21bcab26440a24daace50d4a3ebef181be5f0e8c
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-04 09:36:31 -0400

    Added the photo grid, added some styling

--- src/Pages/OurStory/OurStory.js
+++ src/Pages/OurStory/OurStory.js
@@ -3,15 +3,20 @@ import React from 'react'
 import "./OurStory.css"
 
 // Import images
-import Runaway from "../../Assets/img/story/1.JPG";
-import FirstDate from "../../Assets/img/story/2.jpeg";
+import Ring from "../../Assets/img/story/ring.jpg"
+import Runaway from "../../Assets/img/story/runaway.JPG";
+import FirstDate from "../../Assets/img/story/firstdate.jpg";
+import Proposal from "../../Assets/img/story/proposal.jpg"
+import Disneyland from "../../Assets/img/story/disneyland.jpg"
+import YachtClub from "../../Assets/img/story/yachtclub.jpg"
+
 
 const OurStory = () => {
     return (
-        <div>
+        <div id="storySection">
             <h1 id="storyHeader">Our Story</h1>
 
-            <div id="storySection">
+            <div id="storyContent">
                 <div id="timeline">
                     <ul className="sessions">
                         <li>
@@ -35,6 +40,25 @@ const OurStory = () => {
                             <p>Got Engaged</p>
                         </li>
                     </ul>
+                </div>
+                <img src={Ring} id="ringPhoto" alt="ring image"></img>
+            </div>
+
+            <div id="imageGrid">
+                <div className="imageGridItem">
+                    <img src={Runaway} alt="" className="gridImage"></img>
+                </div>
+                <div className="imageGridItem">
+                    <img src={FirstDate} alt="" className="gridImage"></img>
+                </div>
+                <div className="imageGridItem">
+                    <img src={Proposal} alt="Proposal" className="gridImage"></img>
+                </div>
+                <div className="imageGridItem">
+                    <img src={Disneyland} alt="Disneyland" className="gridImage"></img>
+                </div>
+                <div className="imageGridItem">
+                    <img src={YachtClub} alt="YachtClub" className="gridImage"></img>
                 </div>
             </div>
         </div>
