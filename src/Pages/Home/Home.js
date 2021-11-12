ab4c1d849cd2d5e5e2cf7fc24e33fd5dea8a994e
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-11 21:21:31 -0500

    Added invite to homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,14 +4,23 @@ import "./Home.css"
 const Home = () => {
     return (
         <div>
-            <iframe
-            id="engagementVideo"
-            
-            src="https://www.youtube.com/embed/Ewez_nEhT2w?autoplay=1&loop=1" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            ></iframe>
+            <div id="weddingInfo">
+                <div id="inviteHeader">
+                    <p id="weddingDate">March 12, 2022</p>
+                    <p className="weddingInvite">The Country Club of Detroit
+                        <br></br>
+                        220 Country Club Dr, Grosse Pointe Farms, MI 48236
+                        <br></br>
+                        Attire: Black Tie
+                    </p>
+                </div>
+                <div id="time">
+                    <p className="weddingInvite">4 o'clock in the afternoon</p>
+                </div>
+                <div>
+                    <p className="weddingInvite">Reception to follow</p>
+                </div>
+            </div>
         </div>
     )
 }
