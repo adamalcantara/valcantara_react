cfeb66e132e7f77370024b628cf5e471a9e3440d
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-11 21:23:29 -0500

    Updated text

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,16 +9,16 @@ const Home = () => {
                     <p id="weddingDate">March 12, 2022</p>
                     <p className="weddingInvite">The Country Club of Detroit
                         <br></br>
-                        220 Country Club Dr, Grosse Pointe Farms, MI 48236
+                        220 Country Club Drive, Grosse Pointe Farms, Michigan 48236
                         <br></br>
                         Attire: Black Tie
                     </p>
                 </div>
                 <div id="time">
-                    <p className="weddingInvite">4 o'clock in the afternoon</p>
+                    <p className="weddingInvite">Half Past Four In The Afternoon</p>
                 </div>
                 <div>
-                    <p className="weddingInvite">Reception to follow</p>
+                    <p className="weddingInvite">Reception To Follow</p>
                 </div>
             </div>
         </div>
