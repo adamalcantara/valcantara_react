7ad30ee4f876474ff1d7e49ace09ee9386284e2c
adamalcantara <alcantaradevelopment@gmail.com>
2021-11-25 19:41:18 -0500

    Redid homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,26 +5,26 @@ const Home = () => {
     return (
         <div id="homePage">
             <div id="video">
-            <iframe id="engagementVideo" src="https://www.youtube.com/embed/Ewez_nEhT2w?controls=0&autoplay=1&loop=1&playlist=Ewez_nEhT2w" title="YouTube video player" frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-            allowfullscreen></iframe>
+                <iframe id="engagementVideo" src="https://www.youtube.com/embed/Ewez_nEhT2w?controls=0&autoplay=1&loop=1&playlist=Ewez_nEhT2w" title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
             </div>
 
-            <div id="weddingInfo">
-                <div id="inviteHeader">
-                    <p id="weddingDate">March 12, 2022</p>
-                    <p className="weddingInvite">The Country Club of Detroit
+            <div id="theWeddingInfo">
+                <div id="weddingInfoLeft">
+                    <p id="theWeddingDate">March 12, 2022</p>
+                    <p id="theWeddingTime">Half Past Four In The Afternoon</p>
+                </div>
+                <div id="weddingInfoRight">
+                    <p className="theWeddingInvite"><span id="CCD">The Country Club of Detroit</span>
                         <br></br>
-                        220 Country Club Drive, Grosse Pointe Farms, Michigan 48236
+                        220 Country Club Drive
                         <br></br>
-                        Attire: Black Tie
+                        Grosse Pointe Farms, Michigan 48236
+                        <p>Reception To Follow
+                            <br></br>
+                            Attire: Black Tie</p>
                     </p>
-                </div>
-                <div id="time">
-                    <p className="weddingInvite">Half Past Four In The Afternoon</p>
-                </div>
-                <div>
-                    <p className="weddingInvite">Reception To Follow</p>
                 </div>
             </div>
         </div>
