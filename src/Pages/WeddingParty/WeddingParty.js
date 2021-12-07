6cf4f601d8e4a4a5ac94be1e8dc4d418c9e90512
adamalcantara <alcantaradevelopment@gmail.com>
2021-12-06 21:15:10 -0500

    Updated wedding party page

--- src/Pages/WeddingParty/WeddingParty.js
+++ src/Pages/WeddingParty/WeddingParty.js
@@ -18,40 +18,40 @@ const WeddingParty = () => {
                     <h3 className="partyName">Monica Pierson</h3>
                     <img src={Monica} className="partyImage" alt="Monica"></img>
                     {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
-                    <ul className="weddingParty">
+                    {/* <ul className="weddingParty">
                         <li className="partyList">Valerie's Sister</li>
                         <li className="partyList">Favorite song: </li>
-                    </ul>
+                    </ul> */}
                 </div>
 
                 <div className="partyMember">
                     <h3 className="partyName">Robert Alcantara</h3>
                     <img src={Robert} className="partyImage" alt="Robert"></img>
                     {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
-                    <ul className="weddingParty">
+                    {/* <ul className="weddingParty">
                         <li className="partyList">Adam's Brother</li>
                         <li className="partyList">Favorite song: </li>
-                    </ul>
+                    </ul> */}
                 </div>
 
                 <div className="partyMember">
                     <h3 className="partyName">Jill Pierson</h3>
                     <img src={Jill} className="partyImage" alt="Jill"></img>
                     {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
-                    <ul className="weddingParty">
+                    {/* <ul className="weddingParty">
                         <li className="partyList">Valerie's Sister</li>
                         <li className="partyList">Favorite song: </li>
-                    </ul>
+                    </ul> */}
                 </div>
 
                 <div className="partyMember">
                     <h3 className="partyName">Nathan Alcantara</h3>
                     <img src={Nathan} className="partyImage" alt="Nathan"></img>
                     {/* <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p> */}
-                    <ul className="weddingParty">
+                    {/* <ul className="weddingParty">
                         <li className="partyList">Adam's Brother</li>
                         <li className="partyList">Favorite song: </li>
-                    </ul>
+                    </ul> */}
                 </div>
 
             </div>
