2c64db566be05af2171fc73750f255acd30cdd92
adamalcantara <alcantaradevelopment@gmail.com>
2021-12-08 20:11:56 -0500

    Added headers to RSVP and Registry.  Changed font

--- src/Pages/RSVP/RSVP.js
+++ src/Pages/RSVP/RSVP.js
@@ -5,6 +5,7 @@ import "./RSVP.css"
 const RSVP = () => {
     return (
         <div>
+            <h1 id="rsvpHeader">RSVP</h1>
             <p id="comingSoon">Coming Soon</p>
         </div>
     )
--- src/Pages/Registry/Registry.js
+++ src/Pages/Registry/Registry.js
@@ -5,6 +5,7 @@ import "./Registry.css"
 const Registry = () => {
     return (
         <div>
+            <h1 id="registryHeader">Registry</h1>
             <p id="comingSoon">Coming Soon</p>
         </div>
     )
